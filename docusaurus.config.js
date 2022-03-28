fea94960dda0a99c0365916765b65386a8959f8a
swaminathanvasanth <vasanth.rajaraman@datakaveri.org>
2022-03-28 16:45:12 +0530

    Updated the file names

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -134,7 +134,7 @@ const config = {
             items: [
               {
                 label: 'Getting Started',
-                to: '/docs/intro',
+                to: '/docs/consumer',
               },
             ],
           },
